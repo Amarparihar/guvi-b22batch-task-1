956a49f68282d0edf159a710c897e1d23047cd02
Amar Parihar <amarparihar1359@gmail.com>
2021-04-29 00:36:11 +0530

    initial message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,162 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './card';
 
 function App() {
+  let carddetails = [
+    {
+      plan : "Free",
+      price : 0 ,
+      period : "month",
+      currency : "$",
+      listItems : [
+        {
+          itemname: "Single User",
+          isDisabled : false
+        },
+
+        {
+          itemname: "5GB Storage",
+          isDisabled : false
+        },
+        {
+          itemname: "Unlimited Public Projects",
+          isDisabled : false
+        },
+        {
+          itemname: "Community Access",
+          isDisabled : false
+        },
+        {
+          itemname: "Unlimited Private Projectsr",
+          isDisabled : "true"
+        },
+        {
+          itemname: "Dedicated Phone Support",
+          isDisabled : "true"
+        },
+        {
+          itemname: "Free Subdomain",
+          isDisabled : "true"
+        },
+        {
+          itemname: "Monthly Status Reports",
+          isDisabled : "true"
+        }
+        
+      ]
+      
+
+
+    },
+
+    {
+      plan : "PLUS",
+      price : 9 ,
+      period : "month",
+      currency : "$",
+      listItems : [
+        {
+          itemname: "5 User",
+          isDisabled : false
+        },
+
+        {
+          itemname: "50GB Storage",
+          isDisabled : false
+        },
+        {
+          itemname: "Unlimited Public Projects",
+          isDisabled : false
+        },
+        {
+          itemname: "Community Access",
+          isDisabled : false
+        },
+        {
+          itemname: "Unlimited Private Projectsr",
+          isDisabled :false
+        },
+        {
+          itemname: "Dedicated Phone Support",
+          isDisabled : false
+        },
+        {
+          itemname: "Free Subdomain",
+          isDisabled : false
+        },
+        {
+          itemname: "Monthly Status Reports",
+          isDisabled : "true"
+        }
+        
+      ]
+      
+
+
+    },
+
+    {
+      plan : "PRO",
+      price : 49 ,
+      period : "month",
+      currency : "$",
+      listItems : [
+        {
+          itemname: "Unlimited  User",
+          isDisabled :false
+        },
+
+        {
+          itemname: "150GB Storage",
+          isDisabled : false
+        },
+        {
+          itemname: "Unlimited Public Projects",
+          isDisabled : false
+        },
+        {
+          itemname: "Community Access",
+          isDisabled : false
+        },
+        {
+          itemname: "Unlimited Private Projectsr",
+          isDisabled : false
+        },
+        {
+          itemname: "Dedicated Phone Support",
+          isDisabled : false
+        },
+        {
+          itemname: "Unlimited Free Subdomain",
+          isDisabled : false
+        },
+        {
+          itemname: "Monthly Status Reports",
+          isDisabled : false
+        }
+        
+      ]
+      
+
+
+    }
+    
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section class="pricing py-5">
+      <div class="container">
+        <div class="row">
+        {
+          carddetails.map((price)=>{
+            return <Card details={price}></Card>
+          })
+        }
+        </div>
+      </div>
+    </section>
+ 
+    
   );
 }
 
