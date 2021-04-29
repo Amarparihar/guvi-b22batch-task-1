7dba3efbb4c0c98fb0ac72ecd51931d4ffd9fddb
Amar Parihar <amarparihar1359@gmail.com>
2021-04-29 21:49:40 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -5,159 +5,318 @@ import Card from './card';
 function App() {
   let carddetails = [
     {
-      plan : "Free",
-      price : 0 ,
-      period : "month",
-      currency : "$",
-      listItems : [
+      plan: "Free",
+      currency: "$",
+      price: 0,
+      period: "month",
+      offers:[
         {
-          itemname: "Single User",
-          isDisabled : false
-        },
-
-        {
-          itemname: "5GB Storage",
-          isDisabled : false
+          name: "Single User",
+          isEnabled: true
         },
         {
-          itemname: "Unlimited Public Projects",
-          isDisabled : false
+          name: "5GB Storage",
+          isEnabled: true
         },
+        
         {
-          itemname: "Community Access",
-          isDisabled : false
+          name: "Unlimited Public Projects",
+          isEnabled: true
         },
+        
         {
-          itemname: "Unlimited Private Projectsr",
-          isDisabled : "true"
+          name: "Community Access",
+          isEnabled: true
         },
+        
         {
-          itemname: "Dedicated Phone Support",
-          isDisabled : "true"
+          name: "Unlimited Private Projects",
+          isEnabled: false
         },
+        
         {
-          itemname: "Free Subdomain",
-          isDisabled : "true"
+          name: "Dedicated Phone Support",
+          isEnabled: false
         },
+        
         {
-          itemname: "Monthly Status Reports",
-          isDisabled : "true"
+          name: "Free Subdomain",
+          isEnabled: false
+        },
+        
+        {
+          name: "Monthly Status Reports",
+          isEnabled: false
         }
         
       ]
-      
-
-
     },
 
     {
-      plan : "PLUS",
-      price : 9 ,
-      period : "month",
-      currency : "$",
-      listItems : [
+      plan: "Plus",
+      currency: "$",
+      price: 9,
+      period: "month",
+      offers:[
         {
-          itemname: "5 User",
-          isDisabled : false
-        },
-
-        {
-          itemname: "50GB Storage",
-          isDisabled : false
+          name: "5 User",
+          isEnabled: true
         },
         {
-          itemname: "Unlimited Public Projects",
-          isDisabled : false
+          name: "50GB Storage",
+          isEnabled: true
         },
+        
         {
-          itemname: "Community Access",
-          isDisabled : false
+          name: "Unlimited Public Projects",
+          isEnabled: true
         },
+        
         {
-          itemname: "Unlimited Private Projectsr",
-          isDisabled :false
+          name: "Community Access",
+          isEnabled: true
         },
+        
         {
-          itemname: "Dedicated Phone Support",
-          isDisabled : false
+          name: "Unlimited Private Projects",
+          isEnabled: true
         },
+        
         {
-          itemname: "Free Subdomain",
-          isDisabled : false
+          name: "Dedicated Phone Support",
+          isEnabled: true
         },
+        
         {
-          itemname: "Monthly Status Reports",
-          isDisabled : "true"
+          name: "Free Subdomain",
+          isEnabled: true
+        },
+        
+        {
+          name: "Monthly Status Reports",
+          isEnabled: false
         }
         
       ]
-      
-
-
     },
 
     {
-      plan : "PRO",
-      price : 49 ,
-      period : "month",
-      currency : "$",
-      listItems : [
+      plan: "Pro",
+      currency: "$",
+      price: 49,
+      period: "month",
+      offers:[
         {
-          itemname: "Unlimited  User",
-          isDisabled :false
-        },
-
-        {
-          itemname: "150GB Storage",
-          isDisabled : false
+          name: "Unlimited User",
+          isEnabled: true
         },
         {
-          itemname: "Unlimited Public Projects",
-          isDisabled : false
+          name: "150GB Storage",
+          isEnabled: true
         },
+        
         {
-          itemname: "Community Access",
-          isDisabled : false
+          name: "Unlimited Public Projects",
+          isEnabled: true
         },
+        
         {
-          itemname: "Unlimited Private Projectsr",
-          isDisabled : false
+          name: "Community Access",
+          isEnabled: true
         },
+        
         {
-          itemname: "Dedicated Phone Support",
-          isDisabled : false
+          name: "Unlimited Private Projects",
+          isEnabled: true
         },
+        
         {
-          itemname: "Unlimited Free Subdomain",
-          isDisabled : false
+          name: "Dedicated Phone Support",
+          isEnabled: true
         },
+        
         {
-          itemname: "Monthly Status Reports",
-          isDisabled : false
+          name: "Free Subdomain",
+          isEnabled: true
+        },
+        
+        {
+          name: "Monthly Status Reports",
+          isEnabled: true
         }
         
       ]
-      
-
-
     }
-    
   ]
   return (
     <section class="pricing py-5">
-      <div class="container">
-        <div class="row">
-        {
-          carddetails.map((price)=>{
-            return <Card details={price}></Card>
-          })
-        }
-        </div>
-      </div>
+    <div class="container">
+    <div class="row">
+      {
+        carddetails.map((items)=>{
+          return <Card data={items}></Card>
+        }) 
+      }
+    </div>
+    </div>
     </section>
- 
-    
   );
 }
 
 export default App;
+
+// let carddetails = [
+//   {
+//     plan : "Free",
+//     price : 0 ,
+//     period : "month",
+//     currency : "$",
+//     listItems : [
+//       {
+//         itemname: "Single User",
+//         isDisabled : false
+//       },
+
+//       {
+//         itemname: "5GB Storage",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Unlimited Public Projects",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Community Access",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Unlimited Private Projectsr",
+//         isDisabled : "true"
+//       },
+//       {
+//         itemname: "Dedicated Phone Support",
+//         isDisabled : "true"
+//       },
+//       {
+//         itemname: "Free Subdomain",
+//         isDisabled : "true"
+//       },
+//       {
+//         itemname: "Monthly Status Reports",
+//         isDisabled : "true"
+//       }
+      
+//     ]
+    
+
+
+//   },
+
+//   {
+//     plan : "PLUS",
+//     price : 9 ,
+//     period : "month",
+//     currency : "$",
+//     listItems : [
+//       {
+//         itemname: "5 User",
+//         isDisabled : false
+//       },
+
+//       {
+//         itemname: "50GB Storage",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Unlimited Public Projects",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Community Access",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Unlimited Private Projectsr",
+//         isDisabled :false
+//       },
+//       {
+//         itemname: "Dedicated Phone Support",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Free Subdomain",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Monthly Status Reports",
+//         isDisabled : "true"
+//       }
+      
+//     ]
+    
+
+
+//   },
+
+//   {
+//     plan : "PRO",
+//     price : 49 ,
+//     period : "month",
+//     currency : "$",
+//     listItems : [
+//       {
+//         itemname: "Unlimited  User",
+//         isDisabled :false
+//       },
+
+//       {
+//         itemname: "150GB Storage",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Unlimited Public Projects",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Community Access",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Unlimited Private Projectsr",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Dedicated Phone Support",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Unlimited Free Subdomain",
+//         isDisabled : false
+//       },
+//       {
+//         itemname: "Monthly Status Reports",
+//         isDisabled : false
+//       }
+      
+//     ]
+    
+
+
+//   }
+  
+// ]
+// return (
+//   <section class="pricing py-5">
+//     <div class="container">
+//       <div class="row">
+//       {
+//         carddetails.map((price)=>{
+//           return <Card details={price}></Card>
+//         })
+//       }
+//       </div>
+//     </div>
+//   </section>
+
+  
+// );
